64ab038e9f1b171de508c02bfcd73697e6b2b2c0
Tyler <tyler.evans116@gmail.com>
2019-01-05 11:33:37 -0700

    add material ui

--- Avocado.Web/ClientApp/src/app/app.module.ts
+++ Avocado.Web/ClientApp/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./heroes/heroes.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })

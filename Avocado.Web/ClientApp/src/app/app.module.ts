232c443b86c8aafc4791ea2a3c27021e3216f5b5
Tyler <tyler.evans116@gmail.com>
2019-01-05 11:34:27 -0700

    remove TOH component

--- Avocado.Web/ClientApp/src/app/app.module.ts
+++ Avocado.Web/ClientApp/src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeroesComponent } from "./heroes/heroes.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]

ce8d63f76080c5384fad57752a333550c3f0c045
Tyler <tyler.evans116@gmail.com>
2019-02-06 08:24:38 -0700

    add: event form stub

--- Avocado.Web/ClientApp/src/app/events/events-routing.module.ts
+++ Avocado.Web/ClientApp/src/app/events/events-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { EventListComponent } from "@avocado/events/components/event-list/event-list.component";
+import { EventFormComponent } from "@avocado/events/components/event-form/event-form.component";
 
-const routes: Routes = [{ path: "", component: EventListComponent }];
+const routes: Routes = [
+  { path: "", component: EventListComponent },
+  { path: "new", component: EventFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- Avocado.Web/ClientApp/src/app/events/events.module.ts
+++ Avocado.Web/ClientApp/src/app/events/events.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 
 import { EventsRoutingModule } from "@avocado/events/events-routing.module";
 import { EventListComponent } from "@avocado/events/components/event-list/event-list.component";
+import { EventFormComponent } from "@avocado/events/components/event-form/event-form.component";
 
 @NgModule({
-  declarations: [EventListComponent],
+  declarations: [EventListComponent, EventFormComponent],
   imports: [CommonModule, EventsRoutingModule]
 })
 export class EventsModule {}

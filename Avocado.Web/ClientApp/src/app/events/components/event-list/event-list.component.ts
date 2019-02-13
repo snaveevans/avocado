454a8d0a6d6f0066f115b7fcfa4408f40e768956
Tyler <tyler.evans116@gmail.com>
2019-02-13 13:42:16 -0700

    add: card example

--- Avocado.Web/ClientApp/src/app/events/components/event-list/event-list.component.ts
+++ Avocado.Web/ClientApp/src/app/events/components/event-list/event-list.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'av-event-list',
-  templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.scss']
+  selector: "av-event-list",
+  templateUrl: "./event-list.component.html",
+  styleUrls: ["./event-list.component.scss"]
 })
 export class EventListComponent implements OnInit {
+  cards = [1, 2, 3, 4, 5, 6];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

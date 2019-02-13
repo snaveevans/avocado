fc31755763d9e6fe5cf41f84f34138d718b12a44
Tyler <tyler.evans116@gmail.com>
2019-02-13 13:42:39 -0700

    fix: missed events module changes

--- Avocado.Web/ClientApp/src/app/events/events.module.ts
+++ Avocado.Web/ClientApp/src/app/events/events.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from "@angular/common";
 import { EventsRoutingModule } from "@avocado/events/events-routing.module";
 import { EventListComponent } from "@avocado/events/components/event-list/event-list.component";
 import { EventFormComponent } from "@avocado/events/components/event-form/event-form.component";
+import { MaterialModule } from "@avocado/material/material.module";
+import { CoreModule } from "@avocado/core/core.module";
 
 @NgModule({
   declarations: [EventListComponent, EventFormComponent],
-  imports: [CommonModule, EventsRoutingModule]
+  imports: [CommonModule, EventsRoutingModule, MaterialModule, CoreModule]
 })
 export class EventsModule {}

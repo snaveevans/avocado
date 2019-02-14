15eb77a8a1a72f67e3f3d905a8c4d11edc103073
Tyler <tyler.evans116@gmail.com>
2019-02-14 16:57:27 -0700

    add: change to properties actual enumeractions

--- Avocado.Domain/Entities/Member.cs
+++ Avocado.Domain/Entities/Member.cs
@@ -7,30 +7,30 @@ namespace Avocado.Domain.Entities
     {
         public Guid AccountId { get; private set; }
         public Guid EventId { get; private set; }
-        public string AttendanceStatus { get; private set; }
-        public string Role { get; private set; }
+        public AttendanceStatuses AttendanceStatus { get; private set; }
+        public Roles Role { get; private set; }
         
         [Obsolete("system constructor")]
         protected Member() { }
         internal Member(Account account, Event evnt, Roles role)
         {
-            if (account == null) { throw new ArgumentNullException(nameof(account));}
-            if (evnt == null) { throw new ArgumentNullException(nameof(evnt));}
+            if (account == null) { throw new ArgumentNullException(nameof(account)); }
+            if (evnt == null) { throw new ArgumentNullException(nameof(evnt)); }
             
             AccountId = account.Id;
             EventId = evnt.Id;
-            AttendanceStatus = AttendanceStatuses.NotResponded.ToString();
-            Role = role.ToString();
+            AttendanceStatus = AttendanceStatuses.NotResponded;
+            Role = role;
         }
 
         internal void UpdateAttendanceStatus(AttendanceStatuses status)
         {
-            AttendanceStatus = status.ToString();
+            AttendanceStatus = status;
         }
 
         internal void UpdateRole(Roles role)
         {
-            Role = role.ToString();
+            Role = role;
         }
     }
 }

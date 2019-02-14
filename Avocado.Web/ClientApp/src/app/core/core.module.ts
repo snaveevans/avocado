ed87ceadaf53ed234994642e0389ec416fa35f9e
Tyler <tyler.evans116@gmail.com>
2019-02-14 11:39:37 -0700

    add: reactive forms, format imports

--- Avocado.Web/ClientApp/src/app/core/core.module.ts
+++ Avocado.Web/ClientApp/src/app/core/core.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { RouterModule } from "@angular/router";
 import { HttpClientModule } from "@angular/common/http";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-
-import { MaterialModule } from "@avocado/material/material.module";
-import { ToolbarComponent } from "@avocado/core/components/toolbar/toolbar.component";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { BottomNavigationComponent } from "@avocado/core/components/bottom-navigation/bottom-navigation.component";
 import { SideNavigationComponent } from "@avocado/core/components/side-navigation/side-navigation.component";
+import { ToolbarComponent } from "@avocado/core/components/toolbar/toolbar.component";
 import { ScrollDirective } from "@avocado/core/directives/scroll.directive";
+import { MaterialModule } from "@avocado/material/material.module";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 const COMPONENTS = [
   ToolbarComponent,
--- Avocado.Web/ClientApp/src/app/events/events-routing.module.ts
+++ Avocado.Web/ClientApp/src/app/events/events-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-
-import { EventListComponent } from "@avocado/events/components/event-list/event-list.component";
+import { RouterModule, Routes } from "@angular/router";
 import { EventFormComponent } from "@avocado/events/components/event-form/event-form.component";
+import { EventListComponent } from "@avocado/events/components/event-list/event-list.component";
 
 const routes: Routes = [
   { path: "", component: EventListComponent },

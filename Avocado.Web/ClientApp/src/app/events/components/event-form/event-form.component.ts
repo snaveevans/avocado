47a0ea3ac7114da272d381d8e82a73b034d903b3
Tyler <tyler.evans116@gmail.com>
2019-02-14 11:39:50 -0700

    add: event form

--- Avocado.Web/ClientApp/src/app/events/components/event-form/event-form.component.ts
+++ Avocado.Web/ClientApp/src/app/events/components/event-form/event-form.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'av-event-form',
-  templateUrl: './event-form.component.html',
-  styleUrls: ['./event-form.component.scss']
+  selector: "av-event-form",
+  templateUrl: "./event-form.component.html",
+  styleUrls: ["./event-form.component.scss"]
 })
 export class EventFormComponent implements OnInit {
+  eventForm = this.formBuilder.group({
+    name: ["", Validators.required],
+    description: ["", Validators.required]
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  handleSubmit(): void {
+    // save the form
+    console.log(`submitted!  isValid: ${this.eventForm.valid}`);
   }
-
 }

7a6eb2c967af011845e2dba24cd5feba400191a9
Tyler <tyler.evans116@gmail.com>
2019-02-19 15:51:34 -0700

    docs: additional swagger definitions

--- Avocado.Web/Models/RegisterModel.cs
+++ Avocado.Web/Models/RegisterModel.cs
@@ -2,12 +2,24 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Avocado.Web.Models
 {
+    /// <summary>
+    /// Structure for registering a new account.
+    /// </summary>
     public class RegisterModel : LoginModel
     {
+        /// <summary>
+        /// Name the new account will be set to.
+        /// </summary>
         [Required]
         public string Name { get; set; }
+        /// <summary>
+        /// UserName the new account will be set to, must app unique.
+        /// </summary>
         [Required]
         public string UserName { get; set; }
+        /// <summary>
+        /// Picture the new account will have (optional).
+        /// </summary>
         public string Picture { get; set; }
     }
 }
